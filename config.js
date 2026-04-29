eef2ffb7301f78bf7f6e6088ef8ab25a87203eec
apna-bazar-vps <apnabazar2203@gmail.com>
2026-04-29 17:04:14 +0530

    fix: base_url + api routes + image handling

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const isLocalhost =
-  window.location.hostname === 'localhost' ||
-  window.location.hostname === '127.0.0.1';
+(function () {
+  const isLocal =
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '127.0.0.1';
 
-const BASE_URL = isLocalhost
-  ? 'http://localhost:5000'
-  : 'http://187.127.146.178:5000';
-
-window.BASE_URL = BASE_URL;
+  window.BASE_URL = isLocal
+    ? 'http://localhost:5000'
+    : 'http://187.127.146.178:5000';
+})();
